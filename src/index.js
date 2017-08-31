c72dff686de9e8a53faadfbb3889688f60cf9b71
Blake Knight <bknight@Blake-Knight.local>
2017-08-31 10:47:00 -0500

    add tests for index; bug fixes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import rethinkdbdash from 'rethinkdbdash';
-import { obj as objectStream } from 'through2';
+import backfillTable from './backfill-table';
 import defaultOptions from './defaultOptions';
-import elasticsearchPath from './elasticsearch-path';
 import ensureTables from './ensure-tables';
 import urlString from './url-string';
+import watchTable from './watch-table';
 
 // The base URL for the Elasticsearch server
 let elasticsearchBaseUrl;
@@ -20,43 +20,36 @@ async function init(extraOptions) {
   elasticsearchBaseUrl = urlString(options.elasticsearch);
   r = rethinkdbdash(options.rethinkdb);
 
+  // If an error is thrown, try to cleanup connection pool before throwing error to handler
   try {
-    ensureConnections(options.elasticsearch);
+    await ensureConnections(options.elasticsearch);
     await ensureTables(r, options.tables);
   } catch (e) {
-    console.error(e.message);
-    return cleanup();
+    await cleanup();
+    throw e;
   }
 
   if (options.watch) {
-    options.tables.forEach(watchTable);
+    options.tables.forEach(tableDetails =>
+      watchTable(r, { baseURL: elasticsearchBaseUrl, ...tableDetails })
+    );
   }
 
   if (options.backfill) {
-    options.tables.forEach(backfillTable);
+    const backfillPromises = options.tables.map(tableDetails =>
+      backfillTable(r, {
+        baseURL: elasticsearchBaseUrl,
+        ...tableDetails
+      })
+    );
+
+    await Promise.all(backfillPromises);
   }
 
   // If nothing is to be done, cleanup connections and exit
-  if (!options.watch && !options.backfill) {
+  if (!options.watch) {
     cleanup();
   }
-}
-
-/**
- * Fill an Elasticsearch type with the contents of a RethinkDB table
- * @param  {String}   db        The database in RethinkDB to duplicate
- * @param  {String}   table     The table in RethinkDB to duplicate
- * @param  {Function} transform (optional) A function to transform the document before storage in Elasticsearch
- */
-async function backfillTable({ db, table, transform }) {
-  const dataStream = r.db(db).table(table).toStream();
-
-  dataStream.pipe(
-    objectStream((chunk, enc, cb) => {
-      saveDocument({ db, document: chunk, table, transform });
-      cb();
-    })
-  );
 }
 
 async function cleanup() {
@@ -71,45 +64,10 @@ async function ensureConnections() {
   const elasticsearchResponse = await axios.get(elasticsearchBaseUrl);
   const elasticsearchHealthy =
     typeof elasticsearchResponse.data.cluster_uuid === 'string';
-  return elasticsearchHealthy;
-}
 
-/**
- * Replicate a document in Elasticsearch
- * @param  {String}   db        The database in RethinkDB the document resides in (used as Elasticsearch index)
- * @param  {Object}   document  The document to save.  This may be transformed by `transform`.
- * @param  {String}   table     The table in RethinkDB the document resides in (used as Elasticsearch type)
- * @param  {Function} transform (optional) A function to transform the document before storage in Elasticsearch
- */
-function saveDocument({ db, document, table, transform }) {
-  // Transform the document if necessary
-  const documentToSave =
-    typeof transform === 'function'
-      ? transform({ db, document, table })
-      : document;
-
-  if (documentToSave == null) return;
-
-  axios.post(elasticsearchPath({ db, table }), documentToSave, {
-    baseURL: elasticsearchBaseUrl
-  });
-}
-
-/**
-* Stream changes from a RethinkDB table into Elasticsearch
-* @param  {String}   db        The database in RethinkDB to stream updates from
-* @param  {String}   table     The table in RethinkDB to stream updates from
-* @param  {Function} transform (optional) A function to transform the document before storage in Elasticsearch
-*/
-async function watchTable({ db, table, transform }) {
-  const dataStream = r.db(db).table(table).changes().toStream();
-
-  dataStream.pipe(
-    objectStream(({ new_val: chunk }, enc, cb) => {
-      saveDocument({ db, document: chunk, table, transform });
-      cb();
-    })
-  );
+  if (!elasticsearchHealthy) {
+    throw new Error('Could not connect to Elasticsearch server');
+  }
 }
 
 export default init;
